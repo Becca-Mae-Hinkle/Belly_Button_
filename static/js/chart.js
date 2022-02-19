429a707eb327a2193c8ddba9114ffa54c6c4db59
Becca-Mae-Hinkle <beccamae123@yahoo.com>
2022-02-19 17:16:03 -0500

    Final Push with images

--- static/js/chart.js
+++ static/js/chart.js
@@ -90,7 +90,7 @@ function buildCharts(patientID) {
             {
                 domain: { x: [0, 1], y: [0, 1] },
                 value: washFreq,
-                title: { text: "Washing Frequency (Times per Week)" },
+                title: { text: "Belly Button Washing Frequency Scrubs per Week" },
                 type: "indicator",
                 mode: "gauge+number",
                 gauge: {
